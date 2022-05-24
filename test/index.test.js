eee571a68a0726bd162f296f5ad8fd9369c0b395
Shazron Abdullah <shazron@gmail.com>
2022-05-24 11:48:19 +0800

    fix: remove extension code, leaving action code only

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 Adobe. All rights reserved.
+Copyright 2022 Adobe. All rights reserved.
 This file is licensed to you under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License. You may obtain a copy
 of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -12,36 +12,61 @@ governing permissions and limitations under the License.
 /* eslint-disable jest/expect-expect */ // => use assert
 
 const helpers = require('yeoman-test')
-const path = require('path')
+const assert = require('yeoman-assert')
+const cloneDeep = require('lodash.clonedeep')
 
-const excshell = require('../index')
+const theGeneratorPath = require.resolve('../index')
 const Generator = require('yeoman-generator')
 
-const composeWith = jest.spyOn(Generator.prototype, 'composeWith')
-beforeAll(() => {
-  // mock implementations
-  composeWith.mockReturnValue(undefined)
-})
-beforeEach(() => {
-  composeWith.mockClear()
-})
-afterAll(() => {
-  composeWith.mockRestore()
-})
+const { constants } = require('@adobe/generator-app-common-lib')
+const { sdkCodes } = constants
 
 describe('prototype', () => {
   test('exports a yeoman generator', () => {
-    expect(excshell.prototype).toBeInstanceOf(Generator)
+    expect(require(theGeneratorPath).prototype).toBeInstanceOf(Generator)
   })
 })
 
 describe('run', () => {
-  test('test basic ext generator', async () => {
-    const options = { 'skip-prompt': true }
-    await helpers.run(excshell)
+  test('--project-name abc --adobe-services analytics,target,campaign-standard', async () => {
+    const options = cloneDeep(global.basicGeneratorOptions)
+    options['project-name'] = 'abc'
+    options['web-src-folder'] = 'web-src'
+    options['adobe-services'] = `${sdkCodes.analytics},${sdkCodes.target},${sdkCodes.campaign}`
+    await helpers.run(theGeneratorPath)
       .withOptions(options)
-    expect(composeWith).toHaveBeenCalledTimes(2)
-    expect(composeWith).toHaveBeenCalledWith(expect.stringContaining(path.normalize('add-action/generic')), expect.any(Object))
-    expect(composeWith).toHaveBeenCalledWith(expect.stringContaining(path.normalize('add-web-assets/exc-react')), expect.any(Object))
+
+    // added files
+    assert.file('web-src/index.html')
+    assert.file('web-src/404.html')
+    assert.file('web-src/src/index.js')
+    assert.file('web-src/src/exc-runtime.js')
+
+    // greats with projectName
+    assert.fileContent('web-src/index.html', '<h1>Welcome to abc!</h1>')
+
+    // make sure calls index.js to get the list of actions
+    assert.fileContent('web-src/index.html', '<script src="./src/index.js"></script>')
+  })
+
+  test('--project-name abc --adobe-services analytics', async () => {
+    const options = cloneDeep(global.basicGeneratorOptions)
+    options['project-name'] = 'abc'
+    options['web-src-folder'] = 'web-src'
+    options['adobe-services'] = `${sdkCodes.analytics}`
+    await helpers.run(theGeneratorPath)
+      .withOptions(options)
+
+    // added files
+    assert.file('web-src/index.html')
+    assert.file('web-src/404.html')
+    assert.file('web-src/src/index.js')
+    assert.file('web-src/src/exc-runtime.js')
+
+    // greats with projectName
+    assert.fileContent('web-src/index.html', '<h1>Welcome to abc!</h1>')
+
+    // make sure calls index.js to get the list of actions
+    assert.fileContent('web-src/index.html', '<script src="./src/index.js"></script>')
   })
 })
